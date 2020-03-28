ca5432677fe320df711c7a8dd9a03ed2e5389de0
ZacharyJacobCollins <ZacharyJacobCollins@gmail.com>
2020-03-28 15:42:54 -0600

    init

---
+++ src/main/java/com/maxar/api/MaxarCodeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.maxar.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxarCodeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxarCodeChallengeApplication.class, args);
+	}
+
+}
